e5d63ae8b5b4683294653000d3d08c8dd7e1bd81
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-19 13:56:00 -0500

    first commit

---
+++ LearnByDoing/LearnByDoingApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearnByDoingApp.swift
+//  LearnByDoing
+//
+//  Created by Isaac Urbina on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LearnByDoingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
