e696e5916e2195d2460367859f0ed4c1f10aa534
Isaac Urbina <7547113+isaacurbina@users.noreply.github.com>
2025-05-21 17:58:23 -0500

    152. 11. How to show Alert window in SwiftUI (#11)

--- LearnByDoing/CardView.swift
+++ LearnByDoing/CardView.swift
@@ -18,6 +18,7 @@ struct CardView: View {
 	@State private var fadeIn: Bool = false
 	@State private var moveDownward: Bool = false
 	@State private var moveUpward: Bool = false
+	@State private var showAlert: Bool = false
 	
 	
 	// MARK: - body
@@ -44,6 +45,7 @@ struct CardView: View {
 			Button(action: {
 				playSound(sound: "sound-chime", type: "mp3")
 				hapticImpact.impactOccurred()
+				showAlert.toggle()
 			}) {
 				HStack {
 					Text(card.callToAction.uppercased())
@@ -87,6 +89,13 @@ struct CardView: View {
 				moveDownward.toggle()
 				moveUpward.toggle()
 			}
+		}
+		.alert(isPresented: $showAlert) {
+			Alert(
+				title: Text(card.title),
+				message: Text(card.message),
+				dismissButton: .default(Text("OK"))
+			)
 		}
 	}
 }
